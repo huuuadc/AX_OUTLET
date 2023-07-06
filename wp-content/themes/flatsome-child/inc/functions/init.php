44c4540f62dcec6e71d0fe580321ec6b783d99fe
Tran Chi Huu <huuuadc@gmail.com>
2023-07-06 16:04:27 +0700

    delete plugin buy now add plugin rest api log

--- wp-content/themes/flatsome-child/inc/functions/init.php
+++ wp-content/themes/flatsome-child/inc/functions/init.php
@@ -4,9 +4,9 @@
  *
  */
 
-require (__DIR__.'/write_log.php';
-require (__DIR__.'/ajax/post_check_connect_ls.php';
-require (__DIR__.'/register_scripts.php';
+require (__DIR__.'/write_log.php');
+require (__DIR__.'/ajax/post_check_connect_ls.php');
+require (__DIR__.'/register_scripts.php');
 require (__DIR__.'/product/product-register-term-brand.php');
 require (__DIR__.'/product/product-register-term-gender.php');
 require (__DIR__.'/product/product-register-term-shop-by.php');
