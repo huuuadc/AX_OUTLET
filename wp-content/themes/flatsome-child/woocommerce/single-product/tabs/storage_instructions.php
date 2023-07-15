744fd56f7c0f5fa4d2d07bb6dca19c9163b6d21d
Tran Chi Huu <huuuadc@gmail.com>
2023-07-16 01:11:07 +0700

    add product tab storage and policy

---
+++ wp-content/themes/flatsome-child/woocommerce/single-product/tabs/storage_instructions.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Description tab
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/tabs/description.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 2.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+global $post;
+
+?>
+
+<p>
+    <strong>Sản phẩm áp dụng:</strong> Tất cả sản phẩm nguyên giá & giảm giá không quá 50%</br>
+    <strong>Sản phẩm không áp dụng:</strong></br>
+    &ensp;•	Đồ lót, đồ tắm</br>
+    &ensp;•	Nước hoa</br>
+    &ensp;•	Phụ kiện: Đồng hồ, mắt kính, trang sức</br>
+    <strong>Thời gian đổi trả:</strong></br>
+    &ensp;•	Sản phẩm bị lỗi do nhà sản xuất: 14 ngày kể từ ngày nhận được sản phẩm</br>
+    &ensp;•	Thay đổi nhu cầu: 07 ngày kể từ ngày nhận được sản phẩm</br>
+    &ensp;Tham khảo chi tiết chính sách đổi trả hàng tại: <a href="/chinh-sach-doi-tra/"><strong>Chính sách đổi trả</strong></a></br>
+</p>
+
