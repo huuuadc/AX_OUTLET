339433feeb878a97b31575a9d11045888501cd1b
Tu_Dev <tu.tran@dafc.com.vn>
2023-07-27 10:59:07 +0700

    #30: update style ui/ux - search box - size chart - product detail

--- wp-content/themes/flatsome-child/inc/shortcode/size-chart.php
+++ wp-content/themes/flatsome-child/inc/shortcode/size-chart.php
@@ -15,7 +15,8 @@ function add_size_chart(){
             $term_id = $term->term_id;
             $size_chart = get_term_meta( $term_id, 'wh_size_chart', true );
             if($size_chart){
-                echo do_shortcode('[button text="Hướng dẫn chọn kích thước" class="btn--size_chart before_add_to_cart_form" link="#size_chart"][lightbox id="size_chart" width="1440px" padding="20px"]<img src="'.$size_chart.'" width="100%" height="auto"/>[/lightbox]');
+                echo '<style>.mfp-close{display:none}.mfp-close.inside{display:block;}</style>';
+                echo do_shortcode('[button text="Hướng dẫn chọn kích thước" class="btn--size_chart before_add_to_cart_form" link="#size_chart"][lightbox id="size_chart" width="1440px" padding="20px"]<button title="Close (Esc)" type="button" class="mfp-close inside"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button><img src="'.$size_chart.'" width="100%" height="auto"/>[/lightbox]');
             }
             break;
         }
