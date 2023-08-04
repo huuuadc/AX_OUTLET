181fe42509b83ce20c0a69b61d2eaf722309777f
Tran Chi Huu <huuuadc@gmail.com>
2023-08-04 16:35:05 +0700

    add filter order status

--- wp-content/themes/flatsome-child/template-parts/dashboard/header.php
+++ wp-content/themes/flatsome-child/template-parts/dashboard/header.php
@@ -12,6 +12,8 @@
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/assets/dashboard/daterangepicker/daterangepicker.css'?>">
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/assets/dashboard/summernote/summernote-bs4.min.css'?>">
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/assets/dashboard/bootstrap-slider/css/bootstrap-slider.min.css'?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/assets/dashboard/select2/css/select2.min.css'?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/assets/dashboard/select2-bootstrap4-theme/select2-bootstrap4.min.css'?>">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/assets/css/admin-dashboard.css'?>">
 
