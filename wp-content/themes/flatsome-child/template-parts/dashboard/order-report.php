1b70ece45280a1d6c11d36dd03a73755ee71b7f1
Tran Chi Huu <huuuadc@gmail.com>
2023-08-08 11:17:09 +0700

    change title tab product

---
+++ wp-content/themes/flatsome-child/template-parts/dashboard/order-report.php
@@ -0,0 +1,24 @@
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Báo cáo đơn hàng</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="/admin-dashboard">Dashboard</a></li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+<div id="card_report" class="card">
+    <div class="card-header">Báo cáo tồn kho</div>
+    <div class="card-footer">
+        <button class="btn btn-primary">Xuất đơn hàng tổng</button>
+        <button class="btn btn-primary">Xuất đơn hàng chi tiết</button>
+        <button class="btn btn-primary">Xuất tồn kho hiện tại</button>
+    </div>
+</div>
