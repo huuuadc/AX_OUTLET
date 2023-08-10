0225abef3dad56a9af8499863dea9f3388e350a5
Tran Chi Huu <huuuadc@gmail.com>
2023-08-10 17:53:14 +0700

    add inventory report

--- wp-content/themes/flatsome-child/template-parts/dashboard/container-wrapper.php
+++ wp-content/themes/flatsome-child/template-parts/dashboard/container-wrapper.php
@@ -48,10 +48,15 @@
         </section>
     <?php endif;?>
 
-    <?php write_log(get_query_var('pagename'));?>
     <?php if (get_query_var('pagename') == 'admin-dashboard' && isset($wp->query_vars['order-report'])) :?>
         <section class="content">
             <?php get_template_part('template-parts/dashboard/order','report') ?>
+        </section>
+    <?php endif; ?>
+
+    <?php if (get_query_var('pagename') == 'admin-dashboard' && isset($wp->query_vars['inventory-report'])) :?>
+        <section class="content">
+            <?php get_template_part('template-parts/dashboard/inventory','report') ?>
         </section>
     <?php endif; ?>
     <!-- ./main content -->
