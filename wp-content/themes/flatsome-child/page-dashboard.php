66f43b7e78628e1ac1f5dd208fab2fca0173b402
Tran Chi Huu <huuuadc@gmail.com>
2023-08-16 15:06:47 +0700

    check role admin dashboard

--- wp-content/themes/flatsome-child/page-dashboard.php
+++ wp-content/themes/flatsome-child/page-dashboard.php
@@ -16,22 +16,28 @@ date_default_timezone_set('Asia/Ho_Chi_Minh');
     <!-- ./dashboard header -->
 
     <?php
-        //Check login
+        //Check user login
         global $wp;
         if (!is_user_logged_in()){
             get_header();
-            echo '<div style="
-                        height: 20vh;
-                        display: flex;
-                        text-align: center;
-                        justify-content: center;
-                        align-items: center;">';
-            echo '<a style="width: 20%" href="/wp-login.php?redirect_to='. home_url( $wp->request ) .'"><button type="button" class="btn btn-block btn-primary">Login Now</button></a>';
+            echo '<div class="flex-grow justify-center text-center">';
+            echo '<a class="w-25 btn btn-primary m-lg-5 m-sm-2 no-wrap" href="/wp-login.php?redirect_to='. home_url( $wp->request ) .'">Đăng nhập ngay</a>';
             echo '</div>';
             get_footer();
             die;
         }
 
+        //Check permission user login
+        if (!current_user_can('admin_dashboard')){
+            get_header();
+            echo '<div class="flex-grow justify-center text-center" >';
+            echo '<h3 class="text-danger pt-3">Cảnh báo!. Bạn không có quyền truy cập trang này.</h3>';
+            echo '<h3 class="text-danger pb-3">Xin liên hệ quản lý trang web</h3>';
+            echo '<a class="w-25 btn btn-primary m-lg-5 m-sm-2 no-wrap" href="/">Trang Chủ</a>';
+            echo '</div>';
+            get_footer();
+            die;
+        }
     ?>
 
     <!-- Content Wrapper. Contains page content -->
