26814c568ded7a3ecf5980c5b2a871fc62e90518
Tu_Dev <tu.tran@dafc.com.vn>
2023-09-14 11:12:17 +0700

    #76: update shipping amount status (cart page)

--- wp-content/themes/flatsome/woocommerce/cart/cart-shipping.php
+++ wp-content/themes/flatsome/woocommerce/cart/cart-shipping.php
@@ -42,7 +42,8 @@ $calculator_text          = '';
 										} else {
 											printf( '<input type="hidden" name="shipping_method[%1$d]" data-index="%1$d" id="shipping_method_%1$d_%2$s" value="%3$s" class="shipping_method" />', $index, esc_attr( sanitize_title( $method->id ) ), esc_attr( $method->id ) ); // WPCS: XSS ok.
 										}
-										printf( '<label class="shipping__list_label" for="shipping_method_%1$s_%2$s">%3$s</label>', $index, esc_attr( sanitize_title( $method->id ) ), wc_cart_totals_shipping_method_label( $method ) ); // WPCS: XSS ok.
+                                        $method_str = str_replace('Tiêu chuẩn:','',wc_cart_totals_shipping_method_label( $method ));
+										printf( '<label class="shipping__list_label" style="justify-content:end;" for="shipping_method_%1$s_%2$s">%3$s</label>', $index, esc_attr( sanitize_title( $method->id ) ), $method_str ); // WPCS: XSS ok.
 										do_action( 'woocommerce_after_shipping_rate', $method, $index );
 										?>
 									</li>
