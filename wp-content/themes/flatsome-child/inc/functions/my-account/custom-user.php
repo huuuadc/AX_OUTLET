0f32cce2cccd8457214ac04b593087f965f52507
Tu_Dev <tu.tran@dafc.com.vn>
2023-09-18 15:12:55 +0700

    #79: update field birthday user profile

--- wp-content/themes/flatsome-child/inc/functions/my-account/custom-user.php
+++ wp-content/themes/flatsome-child/inc/functions/my-account/custom-user.php
@@ -14,3 +14,28 @@ function logout_without_confirm($action, $result)
     }
 }
 
+/*function action_woocommerce_edit_account_form() {
+    woocommerce_form_field( 'user_registration_user_birthday', array(
+        'type'        => 'text',
+        'label'       => __( 'Ngày sinh', 'woocommerce' ),
+        'placeholder' => __( 'ngày/tháng/năm', 'woocommerce' ),
+        'required'    => true,
+    ), get_user_meta( get_current_user_id(), 'user_registration_user_birthday', true ));
+}
+add_action( 'woocommerce_edit_account_form', 'action_woocommerce_edit_account_form' );*/
+
+// Validate - my account
+function action_woocommerce_save_account_details_errors( $args ){
+    if ( isset($_POST['user_registration_user_birthday']) && empty($_POST['user_registration_user_birthday']) ) {
+        $args->add( 'error', __( 'Vui lòng nhập ngày sinh nhật của bạn', 'woocommerce' ) );
+    }
+}
+add_action( 'woocommerce_save_account_details_errors','action_woocommerce_save_account_details_errors', 10, 1 );
+
+// Save - my account
+function action_woocommerce_save_account_details( $user_id ) {
+    if( isset($_POST['user_registration_user_birthday']) && ! empty($_POST['user_registration_user_birthday']) ) {
+        update_user_meta( $user_id, 'user_registration_user_birthday', sanitize_text_field($_POST['user_registration_user_birthday']) );
+    }
+}
+add_action( 'woocommerce_save_account_details', 'action_woocommerce_save_account_details', 10, 1 );
