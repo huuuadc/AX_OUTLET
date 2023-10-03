17f6214be8e8ea06fdde2ddeebd9825c07ad14e8
Tran Chi Huu <huuuadc@gmail.com>
2023-10-03 16:33:14 +0700

    rename func sort

--- wp-content/themes/flatsome-child/inc/functions/product/product-listing-sort.php
+++ wp-content/themes/flatsome-child/inc/functions/product/product-listing-sort.php
@@ -1,32 +1,30 @@
 <?php
 
 
-function add_postmeta_ordering_args( $args_sort_cw, $orderby ) {
-
-    write_log($orderby);
+function add_postmeta_ordering_args( $args_sort) {
 
     $cw_orderby_value = isset( $_GET['orderby'] ) ? wc_clean( $_GET['orderby'] ) :
         apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby' ) );
 
     switch( $cw_orderby_value ) {
         case 'pricesale-desc':
-            $args_sort_cw['orderby'] = 'meta_value_num';
-            $args_sort_cw['order'] = 'DESC';
-            $args_sort_cw['meta_key'] = 'pricesale';
+            $args_sort['orderby'] = 'meta_value_num';
+            $args_sort['order'] = 'DESC';
+            $args_sort['meta_key'] = 'pricesale';
             break;
         case 'pricesale':
-            $args_sort_cw['orderby'] = 'meta_value_num';
-            $args_sort_cw['order'] = 'ASC';
-            $args_sort_cw['meta_key'] = 'pricesale';
+            $args_sort['orderby'] = 'meta_value_num';
+            $args_sort['order'] = 'ASC';
+            $args_sort['meta_key'] = 'pricesale';
             break;
     }
 
-    return $args_sort_cw;
+    return $args_sort;
 }
 
-add_filter( 'woocommerce_get_catalog_ordering_args', 'add_postmeta_ordering_args',10,2 );
+add_filter( 'woocommerce_get_catalog_ordering_args', 'add_postmeta_ordering_args' );
 
-function cw_add_new_postmeta_orderby( $sort_by ) {
+function add_sort_view_orderby( $sort_by ) {
     unset($sort_by['price']);
     unset($sort_by['price-desc']);
     $sort_by['pricesale-desc'] = __( 'Sort by price (desc)', 'woocommerce' );
@@ -34,5 +32,5 @@ function cw_add_new_postmeta_orderby( $sort_by ) {
     return $sort_by;
 }
 
-add_filter( 'woocommerce_default_catalog_orderby_options', 'cw_add_new_postmeta_orderby' );
-add_filter( 'woocommerce_catalog_orderby', 'cw_add_new_postmeta_orderby' );
+add_filter( 'woocommerce_default_catalog_orderby_options', 'add_sort_view_orderby' );
+add_filter( 'woocommerce_catalog_orderby', 'add_sort_view_orderby' );
