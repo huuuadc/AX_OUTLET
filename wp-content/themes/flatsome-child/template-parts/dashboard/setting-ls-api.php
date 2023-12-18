9bce464f8180f6103040888d09853cd7250b8005
Tran Chi Huu <huuuadc@gmail.com>
2023-12-18 09:55:23 +0700

    fix layout config ls

--- wp-content/themes/flatsome-child/template-parts/dashboard/setting-ls-api.php
+++ wp-content/themes/flatsome-child/template-parts/dashboard/setting-ls-api.php
@@ -24,54 +24,54 @@ $access_token_2             =   get_option('wc_settings_tab_ls_access_token_2') 
     <div class="card-body">
         <h4>Config 1</h4>
         <div class="form-group">
-            <label>Config name</label>
+            <label for="wc_settings_tab_config_name">Config name</label>
             <input id="wc_settings_tab_config_name" name="wc_settings_tab_config_name" type="text" class="form-control" value="<?php echo $config_1?>"/>
         </div>
         <div class="form-group">
-            <label>Base url 1</label>
+            <label for="wc_settings_tab_ls_api_url">Base url 1</label>
             <input id="wc_settings_tab_ls_api_url" name="wc_settings_tab_ls_api_url" type="text" class="form-control" value="<?php echo $base_url_1?>"/>
         </div>
         <div class="form-group">
-            <label>Username 1</label>
+            <label for="wc_settings_tab_ls_api_username">Username 1</label>
             <input id="wc_settings_tab_ls_api_username" name="wc_settings_tab_ls_api_username" type="text" class="form-control" value="<?php echo $username_1?>"/>
         </div>
         <div class="form-group">
-            <label>Password 1</label>
+            <label for="wc_settings_tab_ls_api_password">Password 1</label>
             <input id="wc_settings_tab_ls_api_password" name="wc_settings_tab_ls_api_password" type="text" class="form-control" value="<?php echo $password_1?>"/>
         </div>
         <div class="form-group">
-            <label>Location code 1</label>
+            <label for="wc_settings_tab_ls_location_code">Location code 1</label>
             <input id="wc_settings_tab_ls_location_code" name="wc_settings_tab_ls_location_code" type="text" class="form-control" value="<?php echo $location_from_1?>"/>
         </div>
         <div class="form-group">
-            <label>Location code 2 1</label>
+            <label for="wc_settings_tab_ls_location_code2">Location code 2 1</label>
             <input id="wc_settings_tab_ls_location_code2" name="wc_settings_tab_ls_location_code2" type="text" class="form-control" value="<?php echo $location_to_1?>"/>
         </div>
 
         <hr>
         <h4>Config 2</h4>
         <div class="form-group">
-            <label>Config name</label>
+            <label for="wc_settings_tab_config_name_2">Config name</label>
             <input id="wc_settings_tab_config_name_2" name="wc_settings_tab_config_name_2" type="text" class="form-control" value="<?php echo $config_2?>"/>
         </div>
         <div class="form-group">
-            <label>Base url 2</label>
+            <label for="wc_settings_tab_ls_api_url_2">Base url 2</label>
             <input id="wc_settings_tab_ls_api_url_2" name="wc_settings_tab_ls_api_url_2" type="text" class="form-control" value="<?php echo $base_url_2?>"/>
         </div>
         <div class="form-group">
-            <label>Username 2</label>
+            <label for="wc_settings_tab_ls_api_username_2">Username 2</label>
             <input id="wc_settings_tab_ls_api_username_2" name="wc_settings_tab_ls_api_username_2" type="text" class="form-control" value="<?php echo $username_2?>"/>
         </div>
         <div class="form-group">
-            <label>Password 2</label>
+            <label for="wc_settings_tab_ls_api_password_2">Password 2</label>
             <input id="wc_settings_tab_ls_api_password_2" name="wc_settings_tab_ls_api_password_2" type="text" class="form-control" value="<?php echo $password_2?>"/>
         </div>
         <div class="form-group">
-            <label>Location code 2</label>
+            <label for="wc_settings_tab_ls_location_code_2">Location code 2</label>
             <input id="wc_settings_tab_ls_location_code_2" name="wc_settings_tab_ls_location_code_2" type="text" class="form-control" value="<?php echo $location_from_2?>"/>
         </div>
         <div class="form-group">
-            <label>Location code 2 2</label>
+            <label for="wc_settings_tab_ls_location_code2_2">Location code 2 2</label>
             <input id="wc_settings_tab_ls_location_code2_2" name="wc_settings_tab_ls_location_code2_2" type="text" class="form-control" value="<?php echo $location_to_2?>"/>
         </div>
 
